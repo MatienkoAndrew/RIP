c061df765a0f037fc948007adb4c011d02acca56
First Geruss <fgeruss@at-n2.21-school.ru>
2019-09-16 21:38:19 +0300

    Lab_2

--- lab_2/ex_3.py
+++ lab_2/ex_3.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+import math
 
 data = [4, -30, 100, -100, 123, 1, 0, -1, -4]
 # Реализация задания 3
+def is_module(x):
+    if x < 0:
+        return 0
+    else:
+        return 1
+
+# for i in range(len(data)):
+#     data[i] = is_module(data[i])
+#     print(data[i])
+print(sorted([data[x] for x in range(len(data)) if is_module(data[x])]))
