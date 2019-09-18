2d281597434e524d3cf789123f629012b01c9782
First Geruss <fgeruss@at-n2.21-school.ru>
2019-09-18 16:24:47 +0300

    Lab_2

--- lab_2/librip/ctxmngrs.py
+++ lab_2/librip/ctxmngrs.py
@@ -6,3 +6,24 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+
+# def timer():
+# 	import time
+#
+# 	def wrapper(*args, **kwargs):
+# 		t = time.clock()
+# 		res = func(*args, **kwargs)
+# 		print(func.__name__, time.clock() - t)
+# 		return res
+# 	return wrapper
+
+# Декоратор, вычисляющий время выполнения функции
+
+def timer(func):
+	import time
+
+	def wrapper(*args, **kwargs):
+		t = time.time()
+		res = func(*args, **kwargs)
+		print("Function " + str(func.__name__) + " : " + str(time.time() - t) + "(sec)")
+	return wrapper
