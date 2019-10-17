73e1f09a7011cbdb41d1e67e2ad7f04b767b62db
First Geruss <fgeruss@at-o3.21-school.ru>
2019-10-17 18:47:20 +0300

    Works

--- lab_4/mysite/my_app/urls.py
+++ lab_4/mysite/my_app/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.get_list, name='get_list'),
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- lab_4/mysite/my_app/views.py
+++ lab_4/mysite/my_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 # Create your views here.
-
 #def	index(request):
 #	return HttpResponse("Hello, World! I'm at the 'my_app' repository")
 
@@ -14,3 +14,8 @@ from django.utils import timezone
 def	get_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'example.html', {'posts': posts})
+
+def	post_detail(request, pk):
+	Post.objects.get(pk=pk)
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'post_detail.html', {'post' : post})
